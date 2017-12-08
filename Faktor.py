80c72c3b33a683020b2890c9a6fc4e61403a65de
Andrew Fidel <afidel1743@gmail.com>
2017-12-08 13:01:17 -0800

    new problem / must fix

---
+++ Faktor.py
@@ -0,0 +1,24 @@
+"""
+Faktor
+
+Input: the number of articles you plan to publish
+       The impact Factor the owners require
+
+Output: The minimal number of scientists you need to bribe (Minimum total count of citations)
+
+https://open.kattis.com/problems/faktor
+"""
+
+# num_articles_to_publish = int(input())
+# impact_factor_required = input(input())
+
+num_articles_to_publish = 38
+impact_factor_required = 24
+citations = 1
+output = 0
+
+while output < impact_factor_required:
+    output = citations/num_articles_to_publish
+    citations += 1
+
+print(output)
