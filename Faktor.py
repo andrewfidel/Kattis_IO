c34f42abc8f7e70f9b958c737aed4409538e42a8
Andrew Fidel <afidel1743@gmail.com>
2017-12-08 13:40:03 -0800

    fixed

--- Faktor.py
+++ Faktor.py
@@ -1,3 +1,4 @@
+
 """
 Faktor
 
@@ -12,13 +13,24 @@ https://open.kattis.com/problems/faktor
 # num_articles_to_publish = int(input())
 # impact_factor_required = input(input())
 
-num_articles_to_publish = 38
-impact_factor_required = 24
-citations = 1
-output = 0
+# num_articles_to_publish = 38
+# impact_factor_required = 24
+# citations = 1
+# output = 0
+#
+# while output < impact_factor_required:
+#     output = citations/num_articles_to_publish
+#     citations += 1
+#
+# print(output)
 
-while output < impact_factor_required:
-    output = citations/num_articles_to_publish
-    citations += 1
+def main():
+    num_articles_to_publsh = 38
+    impact_factor_required = 24
 
-print(output)
+    impact_factor_required = impact_factor_required - 1
+
+    output = (impact_factor_required * num_articles_to_publsh) + 1
+    print output
+
+main()
